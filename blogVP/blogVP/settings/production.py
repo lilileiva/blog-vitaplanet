cba4329032337a592f3110032c00ad59e3825c4b
Liliana Leiva <liliana-carmen@hotmail.com>
2021-12-02 17:39:18 -0300

    1.0

---
+++ blogVP/blogVP/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['midominio.com.ar']
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    '''base de datos de produccion'''
+    }
+}
